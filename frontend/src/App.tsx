cf30689c9f902c20f038f981a437001676d8fe09
Sevde <sevdeorscelik@gmail.com>
2022-09-07 12:14:43 +0200

    sevde: added bootstrap closes #9

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import Button from 'react-bootstrap/Button'
 
 function App() {
   const [count, setCount] = useState(0);
@@ -7,13 +8,15 @@ function App() {
   return (
     <div className="App">
       <div>
-        <img src="images/logo-teknoza.png" alt="Logo" width="140" height="50" />
+        <img src="images/logo-teknoza.png" alt="Logo" height="50" />
       </div>
       <h1>Teknomarkt for Informatiker</h1>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <Button variant="warning">bootstrap testing button</Button>{' '}
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
