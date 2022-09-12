dabe03da32e14def4a96e08e6c99cef1b9d57135
omer <jagenau49@gmail.com>
2022-09-12 11:54:30 +0200

    omer : header completed

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,32 +1,39 @@
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { solid, regular, brands, icon } from '@fortawesome/fontawesome-svg-core/import.macro'
+
 import './header.scss'
 
 const Header = () => {
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar expand="lg" className='header-navbar'>
             <Container>
-                <Navbar.Brand href="/home"><div className="d-flex flex-column">
+                <Navbar.Brand href="/home"><div className="navbar-brand">
                     <img src="images/logo-teknoza.png" alt="Logo" height="50" className='logoImage' />
                     <b>Electronics for everyone</b>
                 </div></Navbar.Brand>
+                <div className='search-box'>
+                    <i className="fa-solid fa-magnifying-glass"></i>
+                    <input type="text" placeholder="Search..." className="searchInput" />
+                </div>
                 <Navbar.Toggle aria-controls="main-navbar-nav" />
                 <Navbar.Collapse id="main-navbar-nav">
+
                     <Nav className="ms-auto">
-                        <Nav.Link href="/shopping-cart">Shopping Cart</Nav.Link>
+                        <Nav.Link href="/shopping-cart">Shopping Cart <i className="fa-solid fa-cart-shopping"></i></Nav.Link>
+
                         <Nav.Link href="/register">Register</Nav.Link>
+
                         <Nav.Link href="/register">Login</Nav.Link>
 
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+
+
+                        <NavDropdown title={<i className="fa-solid fa-user"></i>} id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Account</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">
-                                Another action
+                                Orders
                             </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                             <NavDropdown.Divider />
                             <NavDropdown.Item href="#action/3.4">
-                                Separated link
+                                Login
                             </NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
