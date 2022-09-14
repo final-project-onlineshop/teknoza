ebb6bfef8be3203b05d836fc71197323eb2747a2
omer <jagenau49@gmail.com>
2022-09-14 12:33:22 +0200

    omer : Login Page created

---
+++ frontend/src/pages/loginPage/LoginPage.tsx
@@ -0,0 +1,31 @@
+import { Button, Container, Form } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import "./loginPage.scss";
+
+const LoginPage = () => {
+  return (
+    <Container className="login-page">
+      <h1>Login</h1>
+      <Form className="login-form">
+        <Form.Group className="mb-3" controlId="formEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Login
+        </Button>
+        <p>
+          New customer? <NavLink to="/register">Create your account</NavLink>
+        </p>
+      </Form>
+    </Container>
+  );
+};
+
+export default LoginPage;
