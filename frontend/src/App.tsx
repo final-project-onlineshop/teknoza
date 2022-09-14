58603955f77f81772e56563f29d85056dad32ee6
omer <jagenau49@gmail.com>
2022-09-14 13:42:43 +0200

    omer : shipping page created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
-
 import HomePage from "./pages/homePage/HomePage";
 import ProductPage from "./pages/productPage/ProductPage";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { Container, } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import "./App.scss";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+import ShippingPage from "./pages/shippingPage/ShippingPage";
 
 function App() {
   return (
@@ -20,6 +20,7 @@ function App() {
             <Container>
               <Routes>
                 <Route path="/home" element={<HomePage />} />
+                <Route path="/shipping" element={<ShippingPage />} />
                 <Route path="/product/:slug" element={<ProductPage />} />
                 <Route path="/" element={<Navigate to="/home" replace />} />
               </Routes>
@@ -29,10 +30,6 @@ function App() {
             <Footer />
           </footer>
         </div>
-
-
-
-
       </div>
     </BrowserRouter>
   );
