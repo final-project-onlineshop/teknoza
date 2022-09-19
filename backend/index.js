e6aa8de406aae119a419e76e9b2e588ee36cff66
Bekir - CDI <b.burakcebecii@gmail.com>
2022-09-19 19:12:51 +0200

    npm i express-session and cooki-parser..

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,9 @@ import express from "express";
 import cors from "cors";
 import path from "path";
 
+import session from "express-session";
+import cookieParser from "cookie-parser";
+
 import userRouter from "./routes/userRoutes.js";
 
 import productRouter from "./routes/productRoutes.js";
@@ -19,8 +22,21 @@ const port = config.PORT || 3484;
 const app = express();
 
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_BASE_URL,
+    methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"],
+    credentials: true,
+  })
+);
+
+app.set("trust proxy", 1);
+
+app.use(cookieParser());
+
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+// app.use(cors());
 
 //test
 app.use("/api/products", productRouter);
