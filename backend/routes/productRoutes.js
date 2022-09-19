b56b850dcf36eccb7dbef383e87cf74207ca26a0
Bekir - CDI <b.burakcebecii@gmail.com>
2022-09-19 11:54:49 +0200

    for testing isAuth and isAdmin in utils.js changed as true..

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import expressAsyncHandler from "express-async-handler";
 import Product from "../models/productModel.js";
-// import { isAuth, isAdmin } from "../utils.js";
+import { isAuth, isAdmin } from "../utils.js";
 
 const productRouter = express.Router();
 
@@ -12,8 +12,8 @@ productRouter.get("/", async (req, res) => {
 
 productRouter.post(
   "/",
-  // isAuth,
-  // isAdmin,
+  isAuth,
+  isAdmin,
   expressAsyncHandler(async (req, res) => {
     const newProduct = new Product({
       name: "sample name " + Date.now(),
@@ -34,8 +34,8 @@ productRouter.post(
 
 productRouter.put(
   "/:id",
-  // isAuth,
-  // isAdmin,
+  isAuth,
+  isAdmin,
   expressAsyncHandler(async (req, res) => {
     const productId = req.params.id;
     const product = await Product.findById(productId);
@@ -59,8 +59,8 @@ productRouter.put(
 
 productRouter.delete(
   "/:id",
-  // isAuth,
-  // isAdmin,
+  isAuth,
+  isAdmin,
   expressAsyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id);
     if (product) {
@@ -74,7 +74,7 @@ productRouter.delete(
 
 productRouter.post(
   "/:id/reviews",
-  // isAuth,
+  isAuth,
   expressAsyncHandler(async (req, res) => {
     const productId = req.params.id;
     const product = await Product.findById(productId);
@@ -112,8 +112,8 @@ const PAGE_SIZE = 3;
 
 productRouter.get(
   "/admin",
-  // isAuth,
-  // isAdmin,
+  isAuth,
+  isAdmin,
   expressAsyncHandler(async (req, res) => {
     const { query } = req;
     const page = query.page || 1;
--- backend/utils.js
+++ backend/utils.js
@@ -17,30 +17,33 @@ export const generateToken = (user) => {
   );
 };
 
-export const isAuth = (req, res, next) => {
-  const authorization = req.headers.authorization;
-  if (authorization) {
-    const token = authorization.slice(7, authorization.length); // Bearer XXXXXX
-    jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
-      if (err) {
-        res.status(401).send({ message: "Invalid Token" });
-      } else {
-        req.user = decode;
-        next();
-      }
-    });
-  } else {
-    res.status(401).send({ message: "No Token" });
-  }
-};
+export const isAuth = true;
+export const isAdmin = true;
 
-export const isAdmin = (req, res, next) => {
-  if (req.user && req.user.isAdmin) {
-    next();
-  } else {
-    res.status(401).send({ message: "Invalid Admin Token" });
-  }
-};
+// export const isAuth = (req, res, next) => {
+//   const authorization = req.headers.authorization;
+//   if (authorization) {
+//     const token = authorization.slice(7, authorization.length); // Bearer XXXXXX
+//     jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
+//       if (err) {
+//         res.status(401).send({ message: "Invalid Token" });
+//       } else {
+//         req.user = decode;
+//         next();
+//       }
+//     });
+//   } else {
+//     res.status(401).send({ message: "No Token" });
+//   }
+// };
+
+// export const isAdmin = (req, res, next) => {
+//   if (req.user && req.user.isAdmin) {
+//     next();
+//   } else {
+//     res.status(401).send({ message: "Invalid Admin Token" });
+//   }
+// };
 
 // export const mailgun = () =>
 //   mg({
