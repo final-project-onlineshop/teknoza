261fbeba8eba8e3985a384ac1ab5fd60eb7d04f3
Bekir - CDI <b.burakcebecii@gmail.com>
2022-09-21 14:16:22 +0200

    updated .env and config.js

--- backend/config.js
+++ backend/config.js
@@ -1,14 +1,13 @@
 import dotenv from "dotenv";
 const env = dotenv.config(); // parses .env file and stores all variables in process.env
 
-console.log("Loaded Env Config:", env);
-
 export const config = {
   MONGODB_URI: process.env.MONGODB_URI,
   PORT: process.env.PORT,
   JWT_SECRET: process.env.JWT_SECRET,
-  MAILGUN_API_KEY: process.env.MAILGUN_API_KEY,
-  MAILGUN_DOMIAN: process.env.MAILGUN_DOMIAN,
+  MAILER_ACCOUNT_NAME: process.env.MAILER_ACCOUNT_NAME,
+  MAILER_ACCOUNT_PASSWORD: process.env.MAILER_ACCOUNT_PASSWORD,
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
-  FRONTEND_BASE_URL:process.env.FRONTEND_BASE_URL
+  FRONTEND_BASE_URL: process.env.FRONTEND_BASE_URL,
+  SESSION_SECRET: process.env.SESSION_SECRET,
 };
