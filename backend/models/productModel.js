9a12938e40db3692910baf0aaa992f039db5fd31
omer <jagenau49@gmail.com>
2022-09-26 11:12:58 +0200

    dummy data imported

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -15,16 +15,16 @@ const productSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, unique: true },
     slug: { type: String, required: true, unique: true },
-    image: { type: String, required: true },
+    thumbnail: { type: String, required: true },
     images: [String],
     brand: { type: String, required: true },
     category: { type: String, required: true },
     description: { type: String, required: true },
     price: { type: Number, required: true },
-    countInStock: { type: Number, required: true },
-    rating: { type: Number, required: true },
-    numReviews: { type: Number, required: true },
+    stock: { type: Number, required: true },
+   
     reviews: [reviewSchema],
+    discountPercentage: { type: Number },
   },
   {
     timestamps: true,
