d32c201b5b17231149d801cc979ee6cbb9de3032
omer <jagenau49@gmail.com>
2022-09-27 08:39:36 +0200

    productpage updated for be update

--- frontend/src/pages/productPage/ProductPage.tsx
+++ frontend/src/pages/productPage/ProductPage.tsx
@@ -1,12 +1,13 @@
 import "./productPage.scss";
 import { useParams } from "react-router-dom";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useReducer, useState } from "react";
 import axios from "axios";
 import {
   Badge,
   Button,
   Card,
   Col,
+  Container,
   ListGroup,
   ListGroupItem,
   Row,
@@ -17,23 +18,48 @@ import { Store } from "../../Store";
 
 const BASE_API_URL = import.meta.env.VITE_BASE_API_URL;
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, product: action.payload, loading: false };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+
+    default:
+      return state;
+  }
+};
+
 const ProductPage = () => {
+  const [{ loading, error, product }, dispatch] = useReducer(reducer, {
+    product: {},
+    loading: true,
+    error: "",
+  });
+
   const params = useParams();
   const productId = params._id;
-  const [product, setProduct] = useState([]);
+  // const [product, setProduct] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const dataFromApi = await axios.get(
-        `${BASE_API_URL}/products/${productId}`
-      );
+      dispatch({ type: "FETCH_REQUEST" });
 
-      setProduct(dataFromApi.data);
-      // console.log(dataFromApi);
+      try {
+        const dataFromApi = await axios.get(
+          `${BASE_API_URL}/products/${productId}`
+        );
+        dispatch({ type: "FETCH_SUCCESS", payload: dataFromApi.data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: err.message });
+      }
     };
-
     fetchData();
-  }, [productId]);
+    // setProduct(dataFromApi.data);
+    // // console.log(dataFromApi);
+  }, []);
 
   const { state, dispatch: ctxDispatch } = useContext(Store);
 
@@ -45,75 +71,80 @@ const ProductPage = () => {
 
     console.log(cart.cartItems.length);
   };
-
   return (
-    <div className="ProductPage">
+    <Container className="ProductPage">
       <Helmet>
         <title>{product.name}</title>
       </Helmet>
-      <Row>
-        <Col md={6}>
-          <img
-            src={`/images/products/${product.slug}.jpg`}
-            alt={product.name}
-            className="img-large"
-          />
-        </Col>
-        <Col md={3}>
-          <ListGroup variant="flush">
-            <ListGroup.Item>
-              <Helmet>
-                <title>{product.name}</title>
-              </Helmet>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <Rating rating={product.rating} numReviews={product.numReviews} />
-            </ListGroup.Item>
-            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
-            <ListGroup.Item>
-              Description:
-              <p>{product.description}</p>
-            </ListGroup.Item>
-          </ListGroup>
-        </Col>
-        <Col md={3}>
-          <Card>
-            <Card.Body>
-              <ListGroup variant="flush">
-                <ListGroup.Item>
-                  <Row>
-                    <Col>Price:</Col>
-                    <Col>${product.price}</Col>
-                  </Row>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  <Row>
-                    <Col>Status:</Col>
-                    <Col>
-                      {product.countInStock > 0 ? (
-                        <Badge bg="success"> In Stock </Badge>
-                      ) : (
-                        <Badge bg="danger">Unavailable</Badge>
-                      )}
-                    </Col>
-                  </Row>
-                </ListGroup.Item>
-
-                {product.countInStock > 0 && (
+      {loading ? (
+        <p>LOADING...</p>
+      ) : error ? (
+        <p>ERROR!</p>
+      ) : (
+        <Row>
+          <Col md={6}>
+            <img
+              src={product.thumbnail}
+              alt={product.name}
+              className="img-large"
+            />
+          </Col>
+          <Col md={3}>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Helmet>
+                  <title>{product.name}</title>
+                </Helmet>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Rating reviews={product.reviews} />
+              </ListGroup.Item>
+              <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+              <ListGroup.Item>
+                Description:
+                <p>{product.description}</p>
+              </ListGroup.Item>
+            </ListGroup>
+          </Col>
+          <Col md={3}>
+            <Card>
+              <Card.Body>
+                <ListGroup variant="flush">
                   <ListGroup.Item>
-                    <div className="d-grid">
-                      <Button onClick={addToCartHandler} variant="primary">
-                        Add to Cart
-                      </Button>
-                    </div>
+                    <Row>
+                      <Col>Price:</Col>
+                      <Col>${product.price}</Col>
+                    </Row>
                   </ListGroup.Item>
-                )}
-              </ListGroup>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </div>
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Status:</Col>
+                      <Col>
+                        {product.stock > 0 ? (
+                          <Badge bg="success"> In Stock </Badge>
+                        ) : (
+                          <Badge bg="danger">Unavailable</Badge>
+                        )}
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+
+                  {product.stock > 0 && (
+                    <ListGroup.Item>
+                      <div className="d-grid">
+                        <Button onClick={addToCartHandler} variant="primary">
+                          Add to Cart
+                        </Button>
+                      </div>
+                    </ListGroup.Item>
+                  )}
+                </ListGroup>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      )}
+    </Container>
   );
 };
 
