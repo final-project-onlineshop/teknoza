0f7f6e11cefc9367c71641d766d85f8ff8869a40
omer <jagenau49@gmail.com>
2022-10-04 12:06:29 +0200

    omer : fixed, add same item  to cart

--- frontend/src/pages/cartPage/CartPage.tsx
+++ frontend/src/pages/cartPage/CartPage.tsx
@@ -1,48 +1,42 @@
-import { Button, Container } from "react-bootstrap";
+import { useContext } from "react";
+import { Button, Col, Container, ListGroup, Row } from "react-bootstrap";
 import { Helmet } from "react-helmet-async";
+import { Link } from "react-router-dom";
 import MessageBox from "../../components/messageBox/MessageBox";
 import ProductInCart from "../../components/productInCart/ProductInCart";
+import { Store } from "../../Store";
 import "./cartPage.scss";
 
-//TODO:delete after testing
-const productsInCart = [
-  {
-    name: "HP Curved Monitor",
-    slug: "hp-curved-monitor",
-    price: 150,
-    countInStock: 10,
-    countInCart: 2,
-  },
-  {
-    name: "Microsoft Wireless Keyboard",
-    slug: "microsoft-wireless-keyboard",
-    price: 30,
-    countInStock: 1,
-    countInCart: 1,
-  },
-];
-
 const CartPage = () => {
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  
+
+  const { cartItems } = state.cart;
+
   return (
     <Container className="cart-screen">
       <Helmet>
         <title>Shopping Cart</title>
       </Helmet>
-      <h2>Shopping Cart</h2>
-      <div className="cart-body">
-        <div className="products-in-cart">
-          {productsInCart.length == 0 ? (
+      <h1>Shopping Cart</h1>
+      <Row>
+        <Col md={8} className="products-in-cart">
+          {cartItems.length === 0 ? (
             <MessageBox>
-              <p>
-                Cart is empty. <a href="/">Go Shopping</a>
-              </p>
+              Cart is empty. <Link to="/">Go Shopping</Link>
             </MessageBox>
           ) : (
-            productsInCart.map((productInCart, index) => {
-              return <ProductInCart product={productInCart} />;
-            })
+            <ListGroup>
+              {cartItems.map((cartItem, index) => {
+                return <ProductInCart product={cartItem} key={index} />;
+              })}
+            </ListGroup>
           )}
-        </div>
+        </Col>
+        <Col md={4}></Col>
+      </Row>
+      <div className="cart-body">
+        <div className="products-in-cart"></div>
         <div className="cart-details-box">
           <div className="subtotal">
             <span>Subtotal (0 items) : $0 </span>
