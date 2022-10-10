19e271d8db49d4e47d6caa82741fa4b664a55801
Bekir - CDI <b.burakcebecii@gmail.com>
2022-10-10 11:52:50 +0200

    if product.reviews.length equals o zero..

--- backend/routes/seedRoutes.js
+++ backend/routes/seedRoutes.js
@@ -7,10 +7,13 @@ const seedRouter = express.Router();
 
 seedRouter.get("/", async (req, res) => {
   data.products.forEach((product) => {
-    const averageRating =
-      product.reviews.reduce((acc, review) => (acc += review.rating), 0) /
-      product.reviews.length;
-    product.averageRating = Math.ceil(averageRating * 100) / 100;
+    if (product.reviews.length === 0) product.averageRating = 0;
+    else {
+      const averageRating =
+        product.reviews.reduce((acc, review) => (acc += review.rating), 0) /
+        product.reviews.length;
+      product.averageRating = Math.ceil(averageRating * 100) / 100;
+    }
   });
   await Product.remove({});
   const createdProducts = await Product.insertMany(data.products);
